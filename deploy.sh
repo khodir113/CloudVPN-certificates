6b8661b6fbd7f4d40ca4d399c92d498df92a3a55
Khodir <68503986+khodir113@users.noreply.github.com>
2022-03-08 20:57:21 +0500

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install python3
+brew install python3-pip
+brew install mysql-server
+brew install mysql-client
+brew install apg
+brew install telnet
+brew install tmux
+brew install ipcalc
+
+#  deploy.sh
+#  
+#
+#  Created by Farzod Rustamov on 20/02/22.
+#  
